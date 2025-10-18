52f73421889591c0c0de371425e6c49de89a5797
kiethuynh31 <kietminhlop51.z@gmail.com>
2025-10-18 08:36:07 +0700

    Lab5_TS01297_HuynhMinhKiet

---
+++ Lab5/src/main/java/com/poly/lab5/controller/UploadController.java
@@ -0,0 +1,38 @@
+package com.poly.lab5.controller;
+
+import com.poly.lab5.service.ParamService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.ui.Model;
+import java.io.File;
+
+@Controller
+public class UploadController {
+    @Autowired
+    ParamService paramService;
+
+    @GetMapping("/upload/form")
+    public String form() {
+        return "upload/form";
+    }
+
+    @PostMapping("/upload/save")
+    public String save(@RequestParam("photo") MultipartFile file, Model model) {
+        try {
+          
+            File saved = paramService.save(file, "/uploads/"); 
+
+            String publicUrl = "/uploads/" + saved.getName(); 
+
+            model.addAttribute("message", "Lưu thành công: " + saved.getName());
+            // *** Thêm đường dẫn ảnh vào Model ***
+            model.addAttribute("imageUrl", publicUrl); 
+            
+        } catch (Exception e) {
+            model.addAttribute("message", "Lỗi lưu file!");
+        }
+        return "upload/form";
+    }
+}
--- Lab5/src/test/java/com/poly/Tv00020Lab5ApplicationTests.java
+++ Lab5/src/test/java/com/poly/Tv00020Lab5ApplicationTests.java
@@ -1,13 +1,13 @@
-package com.poly.lab5;
+package com.poly;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Lab5ApplicationTests {
+class Tv00020Lab5ApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
